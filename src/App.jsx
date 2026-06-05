6a9ee6cbf8ba7221ff0550c391bb71af44e473e9
Dzmitry <dimaberry1@gmail.com>
2026-06-05 12:32:35 +0300

    chore: configure Webpack with React, Babel and CSS Loader, add Redux and react-router libraries

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function App() {
+  return (
+    <div>
+      <h1>React with Webpack!</h1>
+    </div>
+  );
+}
---
+++ webpack.config.cjs
@@ -0,0 +1,52 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+
+  entry: "./src/index.js",
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    clean: true,
+    publicPath: "/",
+  },
+
+  devServer: {
+    historyApiFallback: true,
+    port: 3000,
+    hot: true,
+    open: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      filename: "index.html",
+    }),
+  ],
+
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+};
